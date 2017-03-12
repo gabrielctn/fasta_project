3b9fa03010b44a4f331b1a7b7d57a4cdf3245981
Gabriel <gabrielcretin@gmail.com>
2017-03-12 16:40:41 +0100

    Add verbose when processing searchByGeneName()

--- search.c
+++ search.c
@@ -3,22 +3,35 @@
 
 bool searchByGeneName(Seq *s, char *geneName, uint32_t occ){
 
-	bool found = FALSE;
-	int i=0;
+	int i=0, nbFound = 0;
+
+	if(occ == 1)
+		printf("\nRecherche de la 1ere occurence de séquence nucléique dans la base, dont le nom du gène est \"%s\" :\n\n", geneName);
+	else if(occ == 0)
+		printf("\nRecherche de toutes les séquences nucléiques dans la base, dont le nom du gène est \"%s\" :\n\n", geneName);
+	else
+		printf("\nRecherche des %u premières occurences de séquences nucléiques dans la base, dont le nom du gène est \"%s\" :\n\n", occ, geneName);
 
 	while(s->next != NULL){
 		// Affiche toutes les séquences correspondantes (occ = 0) ou juste la 1ère (occ = 1)
 		if((occ == 0 || occ == 1) && strcmp(s->name, geneName) == 0){
 			printSeq(s);
-			found = TRUE;
-			if(occ == 1) return found;
+			nbFound++;
+			if(occ == 1) return nbFound;
 		}
 		// Affiche les n séquences correspondantes
 		if((occ > 1) && (strcmp(s->name, geneName) == 0) && (i++ < occ)){
 				printSeq(s);
-				found = TRUE;
+				nbFound++;
 		}
 		s = s->next;
 	}
-	return found;
+
+	if(nbFound < occ)
+		printf("\nIl n'y a que %d occurences dans la base.\n\n", nbFound);
+
+	if(nbFound == FALSE)
+		printf("Désolé, aucune séquence ne correspond au gène %s\n\n", geneName);
+
+	return nbFound;
 }
