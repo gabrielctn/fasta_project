a3cb2f0143f964fe57b3fd7f11fee3bed4b52f0a
sophielem <sophie.lematre@yahoo.com>
2017-03-15 20:13:05 +0100

    Added search_sequence and search_position function

--- headers/search.h
+++ headers/search.h
@@ -7,6 +7,7 @@
 
 
 bool searchByGeneName(Sequences *s, char *geneName, uint32_t occ);
-
+int search_sequence(Sequences *seq, uint32_t occ, char *search);
+int search_position(Sequences *seq, uint32_t occ, uint32_t position, enum chromosome_t chromosome);
 
 #endif
--- search.c
+++ search.c
@@ -35,3 +35,40 @@ bool searchByGeneName(Sequences *s, char *geneName, uint32_t occ){
 
 	return nbFound;
 }
+
+
+int search_sequence(Sequences *seq, uint32_t occ, char *search){
+  char *rep;
+  uint32_t a=0;
+
+  if(seq==NULL) return 0;
+
+  a=search_sequence(seq->next,occ,search);
+
+  if(a<occ || occ==0){                                            // tant que le nombre d'occurence n'a pas été trouvé ou si occ=0
+    rep=strstr(seq->sequence,search);
+    if(rep!=NULL){
+      printf("La séquence recherchée a été trouvée dans la séquence %s\n", seq->name);
+      return 1+a;
+    }
+  }
+  return a;
+}
+
+// Nécessaire de convertir la réponse de l'utilisateur en type enum
+int search_position(Sequences *seq, uint32_t occ, uint32_t position, enum chromosome_t chromosome){
+  uint32_t a=0;
+
+  if(seq==NULL) return 0;
+
+  a=search_position(seq->next, occ, position, chromosome);
+
+  if(a<occ || occ==0){
+    if(seq->chromosome==chromosome &&
+      seq->start < position && seq->end >position){
+      printf("La position %d recherchée a été trouvée dans la séquence %s\n", position, seq->name);
+      return a+1;
+    }
+  }
+  return a;
+}
