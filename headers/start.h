1170e63c0c53eb4ec1f75fb128738e874f7ddaca
Gabriel <gabrielcretin@gmail.com>
2017-03-17 02:34:43 +0100

    Remove print_usage + defined menu related constants

--- headers/start.h
+++ headers/start.h
@@ -1,9 +1,25 @@
 #ifndef START_H
 #define START_H
 
-void display_usage();
+#define MAX_LENGTH_GENE_NAME 20
+#define MAX_LENGTH_SEARCH_SEQUENCE 100
+#define MAX_LENGTH_SUB_SEQUENCE 50
+#define MAX_LENGTH_CHROMOSOME 10
+#define MAX_LENGTH_DICO_SEARCH_SEQUENCE 500
+#define MAX_LENGTH_DICO_PREFIX_SEARCH 50
+
+
+typedef struct menu {
+	char *searchString; // chaîne de caractères utilisée pour les recherches dans la base ou dictionnaire
+	enum chromosome_t chromosome; // nom du chromosome pour compléter la recherche par position
+	int position; // recherche de séquences par position dans le génome
+	int occ; // nombre d'occurences des séquences à afficher
+	int transl; // valeur pour choix de traduction
+} Menu;
+
+
 void printMenu();
-Menu * menu();
+int menu(Menu *m);
 void freeMenu(Menu *m);
 
 #endif
