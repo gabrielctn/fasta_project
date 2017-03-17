960ab314a1031168acfab139175ff02d29edc45b
Gabriel <gabrielcretin@gmail.com>
2017-03-17 02:41:17 +0100

    new prototypes

--- headers/dictionary.h
+++ headers/dictionary.h
@@ -12,7 +12,8 @@ typedef struct nucleic_dico {
 
 void initNucleicDictionary(Nucleic_Dico *nd, Sequences *s);
 void insertDictionary(Nucleic_Dico *d, char *sequence);
-int searchDictionary(Nucleic_Dico *nd, char *sequence);
+int searchSeqDictionary(Nucleic_Dico *nd, char *sequence);
 int dicoNucleicIndex(Nucleic_Dico *nd, char nucleotide);
+int searchNbPrefixDictionary(Nucleic_Dico *nd, char *sequence);
 
 #endif
