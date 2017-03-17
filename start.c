d8c97429b7e358c111616ddd438af2ccfb118be8
Gabriel <gabrielcretin@gmail.com>
2017-03-17 02:33:26 +0100

    Remove display_usage, no more needed

--- start.c
+++ start.c
@@ -2,21 +2,6 @@
 #include "headers/sequences.h"
 #include "headers/start.h"
 
-void display_usage(){
-
-	printf("\n\n********** USAGE **********\n\n");
-	printf("./projet [-h] [-n OCCURENCES] [-d STRING] [-r STRING] [-t NB_GENES]\n\n");
-
-	printf("  -h					print this help and exit\n");
-	printf("  -n OCCURENCES			set number of occurences to print. VALUES: 0 (all) 1 (first) >1 (n first) DEFAULT is all\n");
-	printf("  -d STRING				string used for search or insertion in dictionary\n");
-	printf("  -r STRING				string (gene name, sequence etc.) used for searching in the file\n");
-	printf("  -t NB_GENES			set number of genes to translate\n\n");
-
-	exit(EXIT_FAILURE);
-}
-
-
 
 void printMenu(){
 
