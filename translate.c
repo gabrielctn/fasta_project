00a0acacaec7d4de4a997e256b881c418fa961ce
sophielem <sophie.lematre@yahoo.com>
2017-03-30 09:41:54 +0200

    U=T for ARNm,change protein's end in translate function

--- translate.c
+++ translate.c
@@ -34,21 +34,30 @@ void compare(char *protein, char *tmp, Code *tab, int k){
 
 
 void translate(Sequences *seq){
-    char tmp[4];
-    Code *tab= initialize();
+    // Création du fichier de sortie pour la traduction
     FILE *fd;
     fd=fopen("data/translation.txt","w");
+    if(fd == NULL)
+        err(EXIT_FAILURE,"Erreur fopen: translation.txt:");
+
+    Code *tab= initialize();
+    char tmp[4];
 
     while(seq!=NULL){
         char *sequence;
         int i,j=0,k=0;
+        size_t a;
 
         sequence=strstr(seq->sequence,"ATG");       // Recherche du codon initiateur et renvoie la séquence à traduire
         if(sequence!=NULL){
             char *protein = (char *)malloc(1);
             do{
                 for(i=0;i<3;i++){                    //On récupère le codon de la séquence
-                    tmp[i]=sequence[j++];
+                     // Pour les ARNm le T est remplacé par un U mais il a les même propriétés que le T, on remplace donc le U par le T pour faciliter la traduction
+                    if(sequence[j]=='U')
+                        tmp[i]='T';
+                    tmp[i]=sequence[j];
+                    j++;
                 }
                 tmp[3]='\0';
                 compare(protein,tmp,tab,k);
@@ -56,7 +65,7 @@ void translate(Sequences *seq){
 
                 // Si le codon est STOP on arrête de traduire la séquence
                 if(protein[k]=='Z'){
-                    protein[k]='\0';
+
                     break;
                 }
                 k++;
@@ -70,12 +79,15 @@ void translate(Sequences *seq){
             }else if(protein[k]!='Z'){
                 fprintf(fd,"Attention, il n'y a pas de codon STOP\n");
                 fprintf(fd,"Traduction du gène %s : \n", seq->name);
+            }else{
+                protein[k]='\0';
+                fprintf(fd,"Traduction du gène %s : \n", seq->name);
             }
-            for(i=0;i<strlen(protein);i++){
-                if(i%61==0 && i!=0){
+            for(a=0;a<strlen(protein);a++){
+                if(a%PROT_LINE_SIZE==0 && a!=0){
                     fputs("\n",fd);
                 }
-                fputc(protein[i],fd);
+                fputc(protein[a],fd);
             }
             fputs("\n\n",fd);
         }
