f5bd6e7295bd145a8ea700cdb19b2a3b42682562
Gabriel <gabrielcretin@gmail.com>
2017-04-08 10:07:43 +0200

    Factorization + simplification of translate() + FIX valgrind screaming

--- headers/translate.h
+++ headers/translate.h
@@ -16,5 +16,10 @@ int isARN(char *sequence);
 char * transcription(char *sequence);
 void compare(char *protein, char *tmp, Code *tab, int k);
 void translate(Sequences *seq, Menu *m);
+int isNucleotide(char nucl);
+void printing(char *sequence, int seq_idx, char *protein, int prot_idx, char *name, FILE *fd);
+char *sequenceToTranslate(Sequences *seq, Menu *m, char *ARNm);
+void verbose(Menu *m);
+char *synthetizeProtein(int seq_idx, int prot_idx, char *sequence, Code *tab);
 
 #endif
--- translate.c
+++ translate.c
@@ -48,16 +48,98 @@ char *transcription(char *sequence) {
     return inversion;
 }
 
-void compare(char *protein, char *tmp, Code *tab, int k) {
+void compare(char *protein, char *tmp, Code *tab, int prot_idx) {
     int i;
 
     for (i = 0; i < 64; i++) {
         if (strcmp(tmp, tab[i].codon) == 0) {
-            protein[k] = tab[i].acid;
+            protein[prot_idx] = tab[i].acid;
             return;
         }
     }
-    protein[k] = '?';
+    protein[prot_idx] = '?';
+}
+
+int isNucleotide(char nucl) {
+    return nucl == 'A' || nucl == 'C' || nucl == 'G' || nucl == 'U' || nucl == 'T';
+}
+
+void printing(char *sequence, int seq_idx, char *protein, int prot_idx, char *name, FILE *fd) {
+    size_t a;
+
+    // Commentaire suivant si la séquence a pu être traduite entièrement ou si un codon STOP est au milieu de la séquence
+    fprintf(fd, "Traduction du gène %s : \n", name);
+    if (isNucleotide(sequence[seq_idx])) {
+        fprintf(fd, "Attention, le codon STOP est prématuré\n");
+    } else if (protein[prot_idx] != '*') {
+        fprintf(fd, "Attention, il n'y a pas de codon STOP\n");
+    }
+    for (a = 0; a < strlen(protein); a++) {
+        if (a % PROT_LINE_SIZE == 0 && a != 0) {
+            fputs("\n", fd);
+        }
+        fputc(protein[a], fd);
+    }
+    fputs("\n\n", fd);
+}
+
+char *sequenceToTranslate(Sequences *seq, Menu *m, char *ARNm) {
+    char *sequence;
+
+    if (m->codingSeq == 'n' && isARN(seq->sequence) == 0) {
+        // Trancription de la séquence ADN en ARN pour ensuite la traduire
+        ARNm = transcription(seq->sequence);
+        sequence = strstr(ARNm, "AUG");
+    } else {
+        char *codon = (m->codingSeq == 'o') ? "ATG" : "AUG";
+        // Recherche du codon initiateur et renvoie la séquence à traduire
+        sequence = strstr(seq->sequence, codon);
+    }
+    return sequence;
+}
+
+void verbose(Menu *m) {
+    if (m->occ == 0) {
+        printf("\nTraduction de toutes les séquences du fichier FASTA\n\n");
+    } else if (m->occ == 1) {
+        printf("\nTraduction de la première séquence du fichier FASTA\n\n");
+    } else {
+        printf("\nTraduction des %d premières séquences du fichier FASTA\n\n", m->occ);
+    }
+}
+
+char *synthetizeProtein(int seq_idx, int prot_idx, char *sequence, Code *tab) {
+    int i;
+    char tmp[4];
+    char *protein = (char *)calloc(2, sizeof(char));
+
+    do {
+        // On récupère le codon de la séquence
+        for (i = 0; i < 3 && isNucleotide(sequence[seq_idx]); i++) {
+            /* Si la séquence donnée est codante, on traduit directement de l'ADN,
+             * il faut donc changer les T en U pour la lecture des codons dans le code génétique */
+            tmp[i] = (sequence[seq_idx] == 'T') ? 'U' : sequence[seq_idx];
+            seq_idx++;
+        }
+        tmp[i] = '\0';
+        compare(protein, tmp, tab, prot_idx);
+        // Si le codon est STOP , représenté par *, on arrête de traduire la séquence
+        if (protein[prot_idx] == '*') {
+            protein[prot_idx + 1] = '\0';
+            return protein;
+        }
+        char *tmpProtein = (char *)realloc(protein, prot_idx + 3);
+        if (NULL == tmpProtein) {
+            err(EXIT_FAILURE, "Erreur realloc protein\n");
+        } else {
+            protein = tmpProtein;
+        }
+        prot_idx++;
+    } while (isNucleotide(sequence[seq_idx])); // Tant que la séquence n'est pas finie, il existe des bases nucléotidiques
+    // En sortant ici, il n'y a pas eu de codon stop, donc d'*
+    protein[prot_idx] = '\0';
+
+    return protein;
 }
 
 void translate(Sequences *seq, Menu *m) {
@@ -72,78 +154,21 @@ void translate(Sequences *seq, Menu *m) {
     }
 
     Code *tab = initialize();
-    char tmp[4];
-    char *protein = NULL;
-
-    if (m->occ == 0) {
-        printf("\nTraduction de toutes les séquences du fichier FASTA\n\n");
-    } else if (m->occ == 1) {
-        printf("\nTraduction de la première séquence du fichier FASTA\n\n");
-    } else {
-        printf("\nTraduction des %d premières séquences du fichier FASTA\n\n", m->occ);
-    }
+    verbose(m);
 
     while (seq != NULL) {
-        char *sequence;
-        int i, j = 0, k = 0;
-        size_t a;
+        char *ARNm = NULL;
+        int seq_idx = 0, prot_idx = 0;
 
-        if (m->codingSeq == 'n' && isARN(seq->sequence) == 0) {
-            char *ARN;
-            // Trancription de la séquence ADN en ARN pour ensuite la traduire
-            char *reverse = transcription(seq->sequence);
-            ARN = strdup(reverse);
-
-            sequence = strstr(ARN, "AUG");
-            free(ARN);
-            free(reverse);
-        } else {
-            char *codon = (m->codingSeq == 'o') ? "ATG" : "AUG";
-            // Recherche du codon initiateur et renvoie la séquence à traduire
-            sequence = strstr(seq->sequence, codon);
-        }
-
-        if (sequence != NULL) {
-            protein = (char *)calloc(1, sizeof(char));
-            do {
-                for (i = 0; i < 3; i++) {                   // On récupère le codon de la séquence
-                    tmp[i] = (sequence[j] == 'T') ? 'U' : sequence[j];
-                    j++;
-                }
-                tmp[3] = '\0';
-                compare(protein, tmp, tab, k);
-
-                // Si le codon est STOP , représenté par *, on arrête de traduire la séquence
-                if (protein[k] == '*') {
-                    break;
-                }
-
-                protein = (char *)realloc(protein, k + 2);
-
-                k++;
-            } while (sequence[j] == 'A' || sequence[j] == 'C' || sequence[j] == 'G' || sequence[j] == 'U' || sequence[j] == 'T');       // Tant que la séquence n'est pas finie
-
-            // Commentaire suivant si la séquence a pu être traduite entièrement ou si un codon STOP est au milieu de la séquence
-            if (sequence[j] == 'A' || sequence[j] == 'C' || sequence[j] == 'G' || sequence[j] == 'U' || sequence[j] == 'T') {
-                fprintf(fd, "Traduction du gène %s : \n", seq->name);
-                fprintf(fd, "Attention, le codon STOP est prématuré\n");
-            } else if (protein[k] != '*') {
-                fprintf(fd, "Traduction du gène %s : \n", seq->name);
-                fprintf(fd, "Attention, il n'y a pas de codon STOP\n");
-            } else {
-                fprintf(fd, "Traduction du gène %s : \n", seq->name);
-            }
-            for (a = 0; a < strlen(protein) && protein[a] != '*'; a++) {
-                if (a % PROT_LINE_SIZE == 0 && a != 0) {
-                    fputs("\n", fd);
-                }
-                fputc(protein[a], fd);
-            }
-            if (protein[a] == '*') {
-                fputs("*", fd);
-            }
-            fputs("\n\n", fd);
+        char *sequence = sequenceToTranslate(seq, m, ARNm);
+        if (sequence != NULL) { // Si on a bien trouvé un codon initiateur
+            char *protein = synthetizeProtein(seq_idx, prot_idx, sequence, tab);
+            // printf("%s\n", protein);
+            printing(sequence, seq_idx, protein, prot_idx, seq->name, fd);
             free(protein);
+        }
+        if (m->codingSeq == 'n') {
+            free(ARNm);
         }
         seq = seq->next;
         ++nbOcc;
@@ -151,9 +176,8 @@ void translate(Sequences *seq, Menu *m) {
             break;
         }
     }
-
     printf("Les séquences ont bien été traduites!\n\n");
 
-    free(tab);
     fclose(fd);
+    free(tab);
 }
